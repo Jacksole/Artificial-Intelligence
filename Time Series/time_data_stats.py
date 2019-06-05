5a8df9dd3cb8c176a6ee264ddea734e1827de799
Le Aundre Jackson <dell.5287@gmail.com>
2019-06-05 08:35:48 -0400

    Modified Files
    Fixed typos and declared variables in time series program

--- Time Series/time_data_stats.py
+++ Time Series/time_data_stats.py
@@ -6,24 +6,17 @@ import pandas as pd
 
 def read_data(input_file):
     input_data = np.loadtxt(input_file, delimiter=None)
+    dates = pd.date_range('1950-01', periods=input_data.shape[0], freq='M')
+    output = pd.Series(input_data[:, index], index=dates)
+    return output
 
-
-dates = pd.date_range('1950-01', periods=input_data.shape[0], freq='M')
-
-output = pd.Series(input_data[:, index], index=dates)
-return output
 
 if __name__ == '__main__':
-
-input_file = "./AO.txt"
-timeseries = read_data(input_file)
-
-plt.figure()
-timeseries.plot()
-plt.show()
+    input_file = "./AO.txt"
+    timeseries = read_data(input_file)
+    plt.show()
 
 timeseries['1980':'1990'].plot()
-<matplotlib.axes._subplots.AxesSubplot at 0xa0e4b00 >
 
 plt.show()
 
